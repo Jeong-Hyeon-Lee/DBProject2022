2bd823a1a087336a99387941d7072bd8a12fafaf
LIM, YEON-WOO <90602936+Lim-YeonWoo@users.noreply.github.com>
2022-06-02 02:14:43 +0900

    Create yw_Main.java

---
+++ yw_Main.java
@@ -0,0 +1,39 @@
+package DB2022TEAM03;
+
+import java.sql.*;
+import javax.swing.*;
+
+import java.awt.*;
+import DB2022TEAM03.GEUNJU.M_MainScreen;
+
+public class YeonWoo extends JFrame{
+
+	//JDBC driver name and database URL
+	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+	static final String DB_URL = "jdbc:mysql://localhost:3306/DB2022Team03"; 
+		
+	//Database user, password
+	static final String  USER = "root";
+	static final String PASSWORD="yeonw00";
+		
+	//Connection conn
+	public static Connection conn;
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Connecting to database...");
+		
+		try {
+			conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+			
+			
+			System.out.println("Login succeds");
+		}
+		catch(SQLException sqle) { 
+			System.out.println("Error: " + sqle);
+		}
+		
+		new StartScreen(conn);
+	}
+
+}
