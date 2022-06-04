c0c2df16a4dfdbc39dbbdcf79cceff77c46e2300
Jeong-Hyeon-Lee <hyeon00@ewhain.net>
2022-06-04 16:06:06 +0900

    주석 수정

--- Gym/G_changePromotionInformation.java
+++ Gym/G_changePromotionInformation.java
@@ -18,21 +18,27 @@ import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 import javax.swing.ScrollPaneConstants;
 
+//헬스장 프로모션 정보 수정하기
 public class G_changePromotionInformation extends JFrame {
 	public G_changePromotionInformation(Connection conn, String gymID) throws SQLException {
 		// TODO Auto-generated constructor stub
 		setTitle("프로모션 정보 수정하기");
+		
+		//DB2022_가격 테이블에서 헬스장 번호가 'gymID'와 같은 tuple의 헬스장 번호를 rs에 저장
+		//헬스장 번호가 DB2022_가격 테이블의 primary key 이므로 rs에 저장되는 tuple은 1개
 		PreparedStatement pStmt1 = conn.prepareStatement("select 헬스장번호 from DB2022_가격 where 헬스장번호 = ?");
 		pStmt1.setString(1, gymID);
 		ResultSet rs = pStmt1.executeQuery();
 
 		while (rs.next()) { //데이터베이스에 없는 헬스장이라면 실행 안됨
-			rs.getString(1);
+			rs.getString(1); //첫번째 column 값(헬스장 번호) String으로 반환
 
+			//DB2022_가격 테이블에서 헬스장 번호가 인자로 받은 헬스장 번호와 같은 tuple의 기타프로모션설명을 promoRS에 저장
 			PreparedStatement pStmt0 = conn.prepareStatement("select 기타프로모션설명 from DB2022_가격 where 헬스장번호 = ?");
 			pStmt0.setString(1, gymID);
 			ResultSet promoRS = pStmt0.executeQuery();
 
+			//프로모션 정보가 길어질 수도 있으므로 JTextArea 사용하고 scroll 기능, 자동 줄바꿈 기능 설정
 			JTextArea promo = new JTextArea(8,20);
 			JScrollPane scroll = new JScrollPane(promo,
 					ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
@@ -40,6 +46,7 @@ public class G_changePromotionInformation extends JFrame {
 
 			promo.setLineWrap(true); // 자동 줄바꿈 기능
 			
+			//기타프로모션 설명을 JTextArea에 추가
 			while (promoRS.next()) {
 				String s = promoRS.getString(1);
 				promo.setText(s);
@@ -47,7 +54,8 @@ public class G_changePromotionInformation extends JFrame {
 				promo.setFont(new Font("맑은 고딕", Font.BOLD, 15));
 				add(promo);
 			}
-
+			
+			//수정하기 버튼
 			JButton button = new JButton("수정하기");
 			add(button);
 
@@ -58,20 +66,22 @@ public class G_changePromotionInformation extends JFrame {
 			setVisible(true);
 
 			// 이벤트 처리
-			button.addActionListener(new ActionListener() {
+			button.addActionListener(new ActionListener() { //수정하기 버튼 누를 시
 				@Override
 				public void actionPerformed(java.awt.event.ActionEvent e) {
 					String newPromotion = JOptionPane.showInputDialog("프로모션 수정: ");
 					
-					if(newPromotion == null || newPromotion.equals(""))
+					if(newPromotion == null || newPromotion.equals("")) //아무것도 안 입력하거나 취소 버튼 누를 경우 오류 방지
 						return;
 					
 					try {
+						//DB2022_가격 테이블에서 헬스장 번호가 'gymID'와 같다면 기타프로모션설명 값을 위에서 입력받은 값으로 update
 						PreparedStatement pStmt2 = conn.prepareStatement("update DB2022_가격 set 기타프로모션설명 = ? where 헬스장번호 = ?");
 						pStmt2.setString(1, newPromotion);
 						pStmt2.setString(2, gymID);
 						pStmt2.executeUpdate();
 						
+						//화면에 보이는 정보 update된 값에 맞게 수정
 						ResultSet promoRS2 = pStmt0.executeQuery();
 						while (promoRS2.next()) {
 							String s = promoRS2.getString(1);
--- Gym/G_selectMenu.java
+++ Gym/G_selectMenu.java
@@ -19,6 +19,7 @@ import javax.swing.JPanel;
 import DB2022Team03.YeonWoo.DeleteScreen;
 import DB2022Team03.YeonWoo.StartScreen;
 
+//관장으로 로그인했을 때 보이는 메뉴 화면
 public class G_selectMenu extends JFrame {
 	
 	// JDBC driver name and database URL
@@ -33,18 +34,21 @@ public class G_selectMenu extends JFrame {
 	
 	public G_selectMenu(String gymID, String gymName) throws SQLException{
 		// TODO Auto-generated constructor stub
-		conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+		conn = DriverManager.getConnection(DB_URL, USER, PASSWORD); //데이터베이스 접속
 		
 		setTitle("헬스장 PT 예약 시스템 - " + gymName);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //프레임 윈도우를 닫으면 프로그램 종료
-
+		
+		//화면 상단
 		JPanel top = new JPanel();
-
+		
+		//"MENU" 글씨 띄우기
 		JLabel selectMenu = new JLabel("MENU");
 		selectMenu.setForeground(new Color(5, 0, 153));
 		selectMenu.setFont(new Font("맑은 고딕", Font.BOLD, 25));
 		top.add(selectMenu);
 		
+		//로그아웃, 탈퇴 버튼
 		JPanel menu0 = new JPanel();
 		JButton logout = new JButton("로그아웃");
 		menu0.add(logout);
@@ -56,11 +60,12 @@ public class G_selectMenu extends JFrame {
 
 		JPanel btnpanel = new JPanel();
 		btnpanel.setLayout(new GridLayout(4, 1));
-
+		
+		//메뉴 버튼
 		JButton menu1 = new JButton("가격 정보 수정하기");
 		JButton menu2 = new JButton("프로모션 정보 수정하기");
-		JButton menu3 = new JButton("트레이너 정보 출력하기");
-		JButton menu4 = new JButton("회원 수 출력하기");
+		JButton menu3 = new JButton("트레이너 정보 보기");
+		JButton menu4 = new JButton("회원 수 보기");
 
 		btnpanel.add(menu1);
 		btnpanel.add(menu2);
@@ -72,12 +77,12 @@ public class G_selectMenu extends JFrame {
 
 		setBounds(200, 200, 400, 250);
 
-		setResizable(false); // 화면 크기 고정하는 작업
+		setResizable(false); // 화면 크기 고정
 
 		setVisible(true);
 
 		// 이벤트 처리
-				menu1.addActionListener(new ActionListener() {
+				menu1.addActionListener(new ActionListener() { //가격 정보 수정하기 버튼 누를 시
 					@Override
 					public void actionPerformed(java.awt.event.ActionEvent e) {
 						try {
@@ -89,7 +94,7 @@ public class G_selectMenu extends JFrame {
 					}
 				});
 
-				menu2.addActionListener(new ActionListener() {
+				menu2.addActionListener(new ActionListener() { //프로모션 정보 수정하기 버튼 누를 시
 
 					@Override
 					public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -102,7 +107,7 @@ public class G_selectMenu extends JFrame {
 					}
 				});
 
-				menu3.addActionListener(new ActionListener() {
+				menu3.addActionListener(new ActionListener() { //트레이너 정보 보기 버튼 누를 시
 
 					@Override
 					public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -116,7 +121,7 @@ public class G_selectMenu extends JFrame {
 					}
 				});
 				
-				menu4.addActionListener(new ActionListener() {
+				menu4.addActionListener(new ActionListener() { //회원 수 보기 버튼 누를 시
 
 					@Override
 					public void actionPerformed(java.awt.event.ActionEvent e) {
@@ -129,21 +134,21 @@ public class G_selectMenu extends JFrame {
 					}
 				});
 
-				logout.addActionListener(new ActionListener() {
+				logout.addActionListener(new ActionListener() { //로그아웃 버튼 누를 시
 
 					@Override
 					public void actionPerformed(java.awt.event.ActionEvent e) {
 						new StartScreen(conn);
-						dispose();
+						dispose(); //현재 창 닫기
 					}
 				});
 				
-				deleteAccount.addActionListener(new ActionListener() {
+				deleteAccount.addActionListener(new ActionListener() { //탈퇴하기 버튼 누를 시
 
 					@Override
 					public void actionPerformed(java.awt.event.ActionEvent e) {
 						new DeleteScreen(conn, "관장", gymID);
-						dispose();
+						dispose(); //현재 창 닫기
 					}
 				});
 			}
