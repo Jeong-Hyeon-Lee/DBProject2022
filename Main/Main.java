cee8f36afe4c664c86c091e4168f17a7e7428c3d
Lim YeonWoo <90602936+Lim-YeonWoo@users.noreply.github.com>
2022-06-05 23:10:53 +0900

    change package name [YeonWoo > Main]

--- Main/Main.java
+++ Main/Main.java
@@ -1,34 +1,32 @@
 package DB2022Team03.YeonWoo;
+
 import java.sql.*;
 import javax.swing.*;
 
-public class yw_Main extends JFrame{
+public class Main extends JFrame {
 
-	//JDBC driver name and database URL
+	// JDBC driver name and database URL
 	static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-	static final String DB_URL = "jdbc:mysql://localhost:3306/DB2022Team03"; 
-		
-	//Database user, password
-	static final String  USER = "DB2022Team03";
-	static final String PASSWORD="DB2022Team03";
-		
-	//Connection conn
+	static final String DB_URL = "jdbc:mysql://localhost:3306/DB2022Team03";
+
+	// Database user, password
+	static final String USER = "DB2022Team03";
+	static final String PASSWORD = "DB2022Team03";
+
+	// Connection conn
 	public static Connection conn;
-	
-	
+
 	public static void main(String[] args) {
 		System.out.println("Connecting to database...");
-		
+
 		try {
 			conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
-			
-			
+
 			System.out.println("Login succeeds");
-		}
-		catch(SQLException sqle) { 
+		} catch (SQLException sqle) {
 			System.out.println("Error: " + sqle);
 		}
-		
+
 		new StartScreen(conn);
 	}
 
